cd150f9aac5ae7bd0740adf90ed901aa229c2541
David Cadena <Cadenadavid25@gmail.com>
2025-09-08 18:40:18 -0600

    Completed Problem 2
    Similer to Problem 1 but with an if statment to seperate ages under 40

--- index.js
+++ index.js
@@ -20,12 +20,18 @@ const users = [
 const NameList = document.createElement('li');
 for (const user of users) {
   console.log(user.name);
-  NameList.textContent = ser.name;
+  NameList.textContent = user.name;
   document.getElementById('names-list').appendChild(NameList.cloneNode(true));
 }
 
 // 2. Print out the names of characters whose age is less than 40 in the console, then render them in the HTML list with id "young-characters-list"
-
+for (const user of users) {
+  if (user.age < 40){
+    console.log(user.name);
+    NameList.textContent = user.name;
+    document.getElementById('young-characters-list').appendChild(NameList.cloneNode(true));
+}
+}
 // 3. Create a reusable function that takes any array and uses logic to render a list of character names in the HTML. Use this function to populate the list with id "function-list"
 
 // 4. Create a function that takes an array and an age threshold parameter. The function should only display characters whose age is below the given number. Render results in the list with id "age-filter-list"
